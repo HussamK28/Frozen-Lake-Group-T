7de15713a4d403f46834169223fcc5d242747a33
Hussam Khan <HussamK2812@gmail.com>
2026-01-12 20:05:09 +0000

    Update main.pymain.py

--- main.pymain.py
+++ main.pymain.py
@@ -1,5 +1,8 @@
+
 from src.environment import FrozenLake
-from src.model_based import policy_iteration, value_iteration, sarsa_learning, q_learning
+from src.model_based import policy_iteration, value_iteration, sarsa, q_learning
+
+
 
 def main():
     lake = [
@@ -9,7 +12,7 @@ def main():
         ['#', '.', '.', '$']
     ]
     
-    env = FrozenLake(lake, slip=0.1, max_steps=16, seed=0)
+    env = FrozenLake(lake, slip=0.1, max_steps=100, seed=42)
     
     print('Model-based algorithms')
     print('')
@@ -24,12 +27,12 @@ def main():
 
     print('')
     print('SARSA')
-    policy, value = sarsa_learning(env, max_episodes=2000, eta=0.1, gamma=0.99, epsilon=0.1, seed=None)
+    policy, value, sarsa_returns = sarsa(env, max_episodes=50000, eta=0.07, gamma=0.90, epsilon=1.0, seed=42)
     env.render(policy, value)
 
     print('')
     print('Q-Learning')
-    policy, value = q_learning(env, max_episodes=2000, eta=0.1, gamma=0.99, epsilon=0.1, seed=None)
+    policy, value, q_returns = q_learning(env, max_episodes=50000, eta=0.07, gamma=0.90, epsilon=1.0, seed=42)
     env.render(policy, value)
 
 if __name__ == "__main__":
